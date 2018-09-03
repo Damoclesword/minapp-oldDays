795e765e39ad7b36d323c4e5485480a61eb144f9
imcavoy <yyijian601@gmail.com>
2018-09-03 16:56:57 +0800

    make music play/pause/stop

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,22 +1,66 @@
 // components/classic/music/index.js
-import { classicBeh } from "../classic-beh";
+import {
+  classicBeh
+} from "../classic-beh";
+
+//获取背景音乐管理播放器
+const mgr = wx.getBackgroundAudioManager();
+
 Component({
   /**
    * 组件的属性列表
    */
   behaviors: [classicBeh],
-  properties: {},
+  properties: {
+    url: {
+      type: String,
+      observer: function () {
+        if (mgr.src != this.properties.url) {
+          this.setData({
+            isPlay: false
+          });
+        }
+      }
+    },
+    title: String
+  },
 
   /**
    * 组件的初始数据
    */
   data: {
-    playSrc: "./images/player@playing.png",
-    pauseSrc: "./images/player@waitting.png"
+    isPlay: false,
+    playSrc: "./images/player@play.png",
+    pauseSrc: "./images/player@pause.png"
+  },
+
+  /**
+   * ready中监听音乐的状态
+   */
+  ready: function () {
+    mgr.onStop(() => {
+      this.setData({
+        isPlay: false
+      })
+    });
   },
 
   /**
    * 组件的方法列表
    */
-  methods: {}
+  methods: {
+    onPlay: function (e) {
+      let isPlay = this.properties.isPlay;
+      if (!isPlay) {
+        mgr.src = this.properties.url;
+        mgr.title = this.properties.title;
+        mgr.play();
+      } else {
+        mgr.pause();
+      }
+      this.setData({
+        isPlay: !this.properties.isPlay
+      });
+    }
+  }
 });
