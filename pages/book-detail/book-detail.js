6bda68c009e5349aee5f963c4d892ec2a5c87c99
imcavoy <yyijian601@gmail.com>
2018-09-18 15:47:00 +0800

    add post comment model & finish its animation

--- pages/book-detail/book-detail.js
+++ pages/book-detail/book-detail.js
@@ -1,11 +1,14 @@
 // pages/book-detail/book-detail.js
 import {
   BookModel
-} from '../../models/book';
+} from "../../models/book";
+import {
+  LikeModel
+} from "../../models/like";
 const bookModel = new BookModel();
+const like = new LikeModel();
 
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -13,7 +16,101 @@ Page({
     book: null,
     comments: [],
     likeStatus: false,
-    likeCount: 0
+    likeCount: 0,
+    posting: false,
+    animationModel: {},
+    animationBg: {}
+  },
+
+  /**
+   * Book-detail - 点赞函数
+   */
+  onLike: function (event) {
+    let behaviour = event.detail.behaviour;
+    let data = {
+      art_id: this.data.book.id,
+      type: 400
+    };
+    like.postLike(behaviour, data, res => {
+      console.log("点赞/取消点赞成功");
+    });
+  },
+
+  /**
+   * 弹出短评编辑框
+   */
+  showPostingTap: function (event) {
+    let that = this;
+    //底部弹出动画    
+    let animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "ease",
+      delay: 0
+    });
+    //overlay透明度动画
+    let animationBg = wx.createAnimation({
+      duration: 500,
+      timingFunction: "ease",
+      delay: 0
+    });
+    this.setData({
+      posting: true,
+    });
+    this.animation = animation;
+    this.animationBg = animationBg;
+    setTimeout(function () {
+      that.fadeIn();
+    }, 200);
+  },
+
+  /**
+   * 对应隐藏短评编辑框
+   */
+  hidePostingTap: function (e) {
+    let that = this;
+    //底部弹出动画    
+    let animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "ease",
+      delay: 0
+    });
+    //overlay透明度动画
+    let animationBg = wx.createAnimation({
+      duration: 500,
+      timingFunction: "ease",
+      delay: 0
+    });
+    this.animation = animation;
+    that.fadeOut();
+    setTimeout(function () {
+      that.setData({
+        posting: false,
+      });
+    }, 300);
+  },
+
+  /**
+   * 淡入动画
+   */
+  fadeIn: function () {
+    this.animation.translateY(0).step();
+    this.animationBg.opacity(0.5).step();    
+    this.setData({
+      animationModel: this.animation.export(),
+      animationBg: this.animationBg.export()
+    });
+  },
+
+  /**
+   * 淡出动画
+   */
+  fadeOut: function () {
+    this.animation.translateY("100%").step();
+    this.animationBg.opacity(0).step();
+    this.setData({
+      animationModel: this.animation.export(),
+      animationBg: this.animationBg.export()
+    });
   },
 
   /**
@@ -42,55 +139,41 @@ Page({
         likeStatus: res.like_status,
         likeCount: res.fav_nums
       });
-    })
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function () {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function () {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function () {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function () {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function () {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function () {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function () {}
+});
