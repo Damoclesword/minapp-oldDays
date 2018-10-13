d41df2755fa02b2ac34ad540dbe331698d4704c5
imcavoy <yyijian601@gmail.com>
2018-10-13 21:08:47 +0800

    [classic page][classic model] use Promise to get data

--- models/classic.js
+++ models/classic.js
@@ -1,66 +1,57 @@
-import {
-  HTTP
-} from "../util/http";
+// import { HTTP } from "../util/http"
+import { HTTP } from "../util/http-p"
+
 class ClassicModel extends HTTP {
-  /**
-   * 获取最新一期期刊
-   * @param {*} sCallBack
-   */
-  getLatest(sCallBack) {
-    this.request({
-      url: "classic/latest",
-      method: "GET",
-      success: res => {
-        sCallBack(res);
-        this._setLatestIndex(res.index);
-        wx.setStorageSync(this._getKey(res.index), res);
-      }
-    });
+  //获取最新一期期刊
+  getLatest() {
+    return this.request({ url: `classic/latest` })
   }
 
   /**
    * 获取前/后一期的期刊
    * @param {当前期刊的序号} index
    * @param {选择前一期/后一期} prevOrNext
-   * @param {回调函数} sCallBack
    */
-  getClassicPage(index, prevOrNext, sCallBack) {
+  getClassicPage(index, prevOrNext) {
     //先缓存后调用API
     let key =
-      prevOrNext == "next" ? this._getKey(index + 1) : this._getKey(index - 1);
-    console.log(key);
-    let storagedClassicPage = wx.getStorageSync(key);
+      prevOrNext == "next" ? this._getKey(index + 1) : this._getKey(index - 1)
+    let storagedClassicPage = wx.getStorageSync(key)
     if (!storagedClassicPage) {
-      this.request({
-        // url: "/classic/" + index + "/" + prevOrNext,
-        url: `/classic/${index}/${prevOrNext}`,
-        method: "GET",
-        success: res => {
-          wx.setStorageSync(this._getKey(res.index), res);
-          sCallBack(res);
-        }
-      });
+      return this.request({ url: `/classic/${index}/${prevOrNext}` })
     } else {
-      sCallBack(storagedClassicPage);
+      return new Promise((resolve, reject) => {
+        resolve(storagedClassicPage)
+      })
     }
   }
 
+  getFavClassicItems() {}
+
+  //设置最新一期期刊缓存
+  setLatestIndex(res) {
+    wx.setStorageSync("latest", res.index)
+    //直接给最新一期上缓存
+    this.setClassicStorage(res)
+  }
+
+  //设置某一期刊缓存
+  setClassicStorage(res) {
+    wx.setStorageSync(this._getKey(res.index), res)
+  }
+
   isFirst(index) {
-    return index === 1 ? true : false;
+    return index === 1 ? true : false
   }
 
   isLatest(index) {
-    let latestIndex = this._getLatestIndex();
-    return index === latestIndex ? true : false;
-  }
-
-  _setLatestIndex(index) {
-    wx.setStorageSync("latest", index);
+    let latestIndex = this._getLatestIndex()
+    return index === latestIndex ? true : false
   }
 
   _getLatestIndex() {
-    let index = wx.getStorageSync("latest");
-    return index;
+    let index = wx.getStorageSync("latest")
+    return index
   }
 
   /**
@@ -68,11 +59,9 @@ class ClassicModel extends HTTP {
    * @param {期刊号} index
    */
   _getKey(index) {
-    let key = "classic-" + index;
-    return key;
+    let key = "classic-" + index
+    return key
   }
 }
 
-export {
-  ClassicModel
-};
+export { ClassicModel }
